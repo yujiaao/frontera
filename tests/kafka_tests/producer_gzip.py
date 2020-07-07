85fdba62b2dc5083dd6ae43f31e8d86021cda9b8
xingwanxiang <xingwanxiang@autohome.com.cn>
2020-07-08 07:12:01 +0800

    more tests

--- tests/kafka_tests/producer_gzip.py
+++ tests/kafka_tests/producer_gzip.py
@@ -3,3 +3,11 @@ from kafka import KafkaProducer
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'], compression_type='gzip')
 future = producer.send('my_topic' ,  key= b'key_3', value= b'value_3', partition= 0)
 future.get(timeout= 10)
+
+'''
+发送msgpack消息
+msgpack为MessagePack的简称，是高效二进制序列化类库，比json高效
+
+producer = KafkaProducer(value_serializer=msgpack.dumps)
+producer.send('msgpack-topic', {'key': 'value'})
+'''
